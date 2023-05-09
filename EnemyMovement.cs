caee142d7013c3ced9a8f5394922275eb5cdcae9
thiliporace <85422792+thiliporace@users.noreply.github.com>
2023-05-09 21:01:22 +0000

    Initial commit

---
+++ EnemyMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform Player;
+    public float UpdateRate = 0.1f;
+    private NavMeshAgent Agent;
+
+    private const string IsWalking = "IsWalking";
+    private const string Jump = "Jump";
+    private const string Landed = "Landed";
+
+    private Coroutine FollowCoroutine;
+
+    private void Awake()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    public void StartChasing()
+    {
+        if (FollowCoroutine == null)
+        {
+            FollowCoroutine = StartCoroutine(FollowTarget());
+        }
+        else
+        {
+            Debug.LogWarning("Called StartChasing on Enemy that is already chasing! This is likely a bug in some calling class!");
+        }
+    }
+
+    private IEnumerator FollowTarget()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(UpdateRate);
+
+        while (gameObject.activeSelf)
+        {
+            Agent.SetDestination(Player.transform.position);
+            yield return Wait;
+        }
+    }
+    
+}
